804e9f112e65080d11a5b82fd43f51b37c2f37ad
Md Omar Faruk Chowdhury <ofcjim16@gmail.com>
2026-03-14 11:11:23 +0600

    potato responsive

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const navToggle = document.getElementById('nav-toggle');
 const navItems = document.querySelector('.nav-items');
 
 navToggle.addEventListener('click', () => {
-    navItems.classList.toggle('active');
+  navItems.classList.toggle('active');
 });
 
 
@@ -169,31 +169,31 @@ document.addEventListener("DOMContentLoaded", () => {
 //vegetables scroll
 
 const swiper = new Swiper('#vegScroll', {
-    loop: true,
-    direction: 'vertical',
-    slidesPerView: 5,
-    centeredSlides: true,
-    spaceBetween: 40,
-    initialSlide: 3,
-    
-    speed: 800,
+  loop: true,
+  direction: 'vertical',
+  slidesPerView: 5,
+  centeredSlides: true,
+  spaceBetween: 40,
+  initialSlide: 3,
 
-    mousewheel: {
-        forceToAxis: true,
-        sensitivity: 1,
-    },
+  speed: 800,
 
-    freeMode: {
-        enabled: true,
-        sticky: true,
-        momentum: true,
-        momentumRatio: 0.5,
-        momentumVelocityRatio: 0.6,
-        momentumBounce: false
-    },
+  mousewheel: {
+    forceToAxis: true,
+    sensitivity: 1,
+  },
 
-    watchSlidesProgress: true,
-    resistanceRatio: 0.85,
+  freeMode: {
+    enabled: true,
+    sticky: true,
+    momentum: true,
+    momentumRatio: 0.5,
+    momentumVelocityRatio: 0.6,
+    momentumBounce: false
+  },
+
+  watchSlidesProgress: true,
+  resistanceRatio: 0.85,
 });
 
 const images = document.querySelectorAll('.veg-image img');
@@ -201,18 +201,18 @@ const images = document.querySelectorAll('.veg-image img');
 
 swiper.on('slideChange', () => {
 
-    const activeSlide = swiper.slides[swiper.activeIndex];
-    const name = activeSlide.dataset.name;
+  const activeSlide = swiper.slides[swiper.activeIndex];
+  const name = activeSlide.dataset.name;
 
 
-    images.forEach(img => {
-        img.classList.toggle('active', img.dataset.name === name);
-    });
+  images.forEach(img => {
+    img.classList.toggle('active', img.dataset.name === name);
+  });
 
 
-    swiper.slides.forEach(slide => {
-        slide.classList.toggle('active', slide === activeSlide);
-    });
+  swiper.slides.forEach(slide => {
+    slide.classList.toggle('active', slide === activeSlide);
+  });
 });
 
 
